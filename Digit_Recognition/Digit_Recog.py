61e8325569309e01f40b1a8f61525925b7dac1b9
DhaivatV <vipatdhaivat25@gmail.com>
2021-12-08 02:04:49 +0530

    commit

--- Digit_Recognition/Digit_Recog.py
+++ Digit_Recognition/Digit_Recog.py
@@ -84,4 +84,22 @@ for step, (batch_x, batch_y) in enumerate(train_data.take(training_steps),1):
         print("Training Epoch: %i, Loss: %f, Accuracy:%f"%(step, loss, accuracy))
 
 pred= neural_net(x_test)
-print("Test Accuracy: %f" % accu(pred, y_test))
+print("Test Accuracy: %f" % accu(pred, y_test   ))
+
+n_images = 200
+test_images = x_test[:n_images]
+test_labels = y_test[:n_images]
+predictions = neural_net(test_images)
+
+for i in range(n_images):
+    model_prediction = np.argmax(predictions.numpy()[i])
+    if (model_prediction == test_labels[i]):
+        plt.imshow(np.reshape(test_images[i], [28, 28]), cmap='gray_r')
+        plt.title("Original Labels: %i,Model prediction: %i" % (test_labels[i],model_prediction))
+        plt.show()
+        # print("Original Labels: %i" % test_labels[i])
+        # print("Model prediction: %i" % model_prediction)
+
+
+
+
